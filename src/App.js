7de14e27f465d4dbf1fdf0feeef73d64656cf1ac
Suzanne-11 <suzannewdmello@gmail.com>
2022-09-06 17:01:27 +0530

    added pdf download option

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {FeaturedMeal, Meal} from './components/index.js'
+
+//Functionalities
+// 1) Search for Meals
+// 2) Display the results
+// 3) Featured meals
+// 4) Favourite Recipes
+
+function App() {
+  return (
+    <div className="App">
+      <FeaturedMeal />
+      
+    </div>
+  );
+}
+
+export default App;
